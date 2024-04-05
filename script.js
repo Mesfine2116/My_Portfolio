8ced73414d9f3775ed7b621bfdf71106aded92d2
Mesfinee <you@ex>
2024-04-06 01:43:34 +0300

    commited

---
+++ script.js
@@ -0,0 +1,117 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        //  sticky navbar on scroll script 
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+
+        //  scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
+
+    //  slide-up script
+
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
+        //  removing smooth scroll on slide-up button click
+        $("html").css("scrollBehavior", "auto");
+    });
+
+    $(".navbar .menu li a").click(function () {
+        //  Smooth scroll on Menu Items click
+
+        $("html").css("scrollBehavior", "smooth");
+    });
+
+    //  Toggle Navbar
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+
+    //  Typing Text Animation 
+
+    var typed = new Typed(".typing", {
+        strings: [
+            "Software Developer",
+            "Front End Developer",
+
+
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: [
+            "Software Developer",
+            "Front End Developer",
+
+
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    //  Owl Carousel  //
+
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
